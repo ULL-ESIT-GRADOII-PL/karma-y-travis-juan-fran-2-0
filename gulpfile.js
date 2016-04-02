aca7f9188f197ec4b85c162a26e0e76e20ab7e0b
alu0100502923 <alu0100502923@ull.edu.es>
2016-04-02 02:55:38 +0100

    corrigiengo fallo en gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var gulp    = require('gulp'),
 var del     = require('del');
 var minifyHTML = require('gulp-minify-html');
 var minifyCSS  = require('gulp-minify-css');
+var karma = require('karma').server;
 
 gulp.task('minify', function () {
   gulp.src('assets/js/*.js')
